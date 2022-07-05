c9eb661bc9d4fd6ce0b7232257a3443da6926889
DESKTOP-ISQQV62\quoci <quocitspkt@gmail.com>
2022-07-05 14:52:12 +0700

    Add project files.

---
+++ WarehouseManagement/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WarehouseManagement.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        #region commands
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        #endregion
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement windowSelected = GetWindowParent(p);
+                var window = windowSelected as Window;
+                if (window != null)
+                {
+                    window.Close();
+                }
+            });
+            MinimizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement windowSelected = GetWindowParent(p);
+                var window = windowSelected as Window;
+                if (window != null)
+                {
+                    if (window.WindowState != WindowState.Minimized)
+                        window.WindowState = WindowState.Minimized;
+                }
+            });
+            MaximizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement windowSelected = GetWindowParent(p);
+                var window = windowSelected as Window;
+                if (window != null)
+                {
+                    if (window.WindowState != WindowState.Maximized)
+                        window.WindowState = WindowState.Maximized;
+                    else
+                        window.WindowState = WindowState.Normal;
+                }
+
+            });
+        }
+        FrameworkElement GetWindowParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+    }
+}
---
+++ WarehouseManagement/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WarehouseManagement.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool Isloaded;
+        public MainViewModel()
+        {
+            if (!Isloaded)
+            {
+                Isloaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.Show();
+            }
+        }
+    }
+}
